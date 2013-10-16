42a37c2b6a51ff7503368ec66b273c76285f9965
wulx <gurdy.woo@gmail.com>
2013-10-16 19:10:28 +0800

    add approximation methods (round and fix)

--- steps_per_time.m
+++ steps_per_time.m
@@ -1,4 +1,4 @@
-function [f_list, dt_list] = steps_per_time(sn_a, sn_c, sn_d, pf_i, pf_m, s_u)
+function [f_list, dt_list] = steps_per_time(sn_a, sn_c, sn_d, pf_i, pf_m, s_u, method)
 %STEPS_PER_TIME steps per time algorithm
 %
 % varargin:
@@ -8,6 +8,7 @@ function [f_list, dt_list] = steps_per_time(sn_a, sn_c, sn_d, pf_i, pf_m, s_u)
 %   pf_i   --   initial pulse frequency
 %   pf_m   --   maximum pulse frequency
 %   s_u    --   unit steps, default value is 1
+%   method --   approximation method
 %
 % varargout:
 %   f_list   --  frequencies list
@@ -17,22 +18,60 @@ function [f_list, dt_list] = steps_per_time(sn_a, sn_c, sn_d, pf_i, pf_m, s_u)
 % last modified by wulx, 2013/10/16
 
 % default settings
+if nargin < 7, method = 'ideal'; end
 if nargin < 6, s_u = 1; end
-sn = sn_a + sn_c + sn_d;
+
+sn = sn_a + sn_d - 1;
 f_list = zeros(1, sn); % pre-allocation for better performance
-dt_list = zeros(1, sn);
+dt_list = zeros(1, sn); % ditto
 
 % unit time period
 dt = s_u / pf_i;
 
-dt_list(1:end) = dt;
-
-% #1 acceleration --------------------------------------------------------%
-% divide frequencies uniformly in accordance with frequency range (pf_i, pf_m) and step numbers (sn_a)
-f_list(1:sn_a) = linspace(pf_i, pf_m, sn_a);
-
-% #2 moving with constant speed ------------------------------------------%
-f_list(sn_a + (1:sn_c)) = pf_m;
-
-% #1 deceleration --------------------------------------------------------%
-f_list(sn_a + sn_c + (1:sn_d)) = linspace(pf_m, pf_i, sn_d);
+switch method
+    case 'ideal'
+        dt_list(1:end) = dt;
+        % #1 acceleration
+        % divide frequencies uniformly in accordance with frequency range (pf_i, pf_m) and step numbers (sn_a)
+        f_list(1:sn_a) = linspace(pf_i, pf_m, sn_a);
+        
+        % #2 moving with constant speed
+        dt_list(sn_a) = (sn_c + 2) * dt;
+        
+        % #3 deceleration
+        % set f_list(sn_a) to pf_m again, duplicated but harmless!
+        f_list(sn_a - 1 + (1:sn_d)) = linspace(pf_m, pf_i, sn_d); 
+    case {'round', 'fix'}
+        % select round method
+        round_to = @(x) round(x);
+        if strcmp(method, 'fix')
+            round_to = @(x) fix(x);
+        end
+        % #1 acceleration
+        % divide frequencies uniformly and round them to nearest integer
+        f_list1 = round_to( linspace(pf_i, pf_m, sn_a) );
+        s_list1 = round_to( dt * f_list1); % step or pulse numbers should be integer
+        dt_list1 = s_list1 ./ f_list1; % actual time periods
+        
+        f_list(1:sn_a-1) = f_list1(1:end-1); % from 1 to sn_a-1
+        dt_list(1:sn_a-1) = dt_list1(1:end-1);
+        
+        % #2 moving with constant speed
+        s_tot2 = round_to( (sn_c + 2) * pf_m * dt ); % total step numbers (rounded to interger)
+        dt2 = s_tot2 / pf_m;
+        
+        f_list(sn_a) = pf_m; % sn_a
+        dt_list(sn_a) = dt2;
+        
+        % #3 deceleration
+        f_list3 = round_to( linspace(pf_m, pf_i, sn_d) );
+        s_list3 = round_to( dt * f_list3 );
+        dt_list3 = s_list3 ./ f_list3;
+        
+        inds = sn_a + (1:sn_d-1); % from sn_a+1 to sn_a+sn_d-1 (i.e. sn)
+        f_list(inds) = f_list3(2:end);
+        dt_list(inds) = dt_list3(2:end);
+    otherwise
+        disp(['unknown approximation method: ' method])
+        disp('supported methods: ideal(default), round and fix')
+end
