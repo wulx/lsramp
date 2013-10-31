8377a6e15f6c762de0ba637a28fba2156e90c217
wulx <gurdy.woo@gmail.com>
2013-10-31 11:19:58 +0800

    revise time_per_step and add tests

---
+++ tests.m
@@ -0,0 +1,29 @@
+%% testing on lsramp
+clear all; close all; clc
+
+% time_per_step
+sn_a = 30;
+sn_c = 10;
+sn_d = 18;
+pf_i = 100;
+pf_m = 1000;
+s_u = 1;
+method = 'round';
+[f_list, dt_list] = time_per_step(sn_a, sn_c, sn_d, pf_i, pf_m, s_u, method);
+
+sn = numel(dt_list);
+t_list = [0 arrayfun(@(n) sum(dt_list(1:n)), 1:sn)];
+f_list2 = [f_list f_list(end)];
+figure, hold on;
+stairs(t_list, f_list2)
+xlim([0 t_list(end)])
+ylim([0 pf_m])
+
+[xb, yb] = stairs(t_list, f_list2);
+
+idx = find(f_list == max(f_list));
+% speed up
+xUp = arrayfun(@(i) t_list(i)+0.5*dt_list(i), 1:idx(1));
+plot(xUp, f_list(1:idx(1)), 'r.')
+
+
--- time_per_step.m
+++ time_per_step.m
@@ -2,12 +2,12 @@ function [f_list, dt_list] = time_per_step(sn_a, sn_c, sn_d, pf_i, pf_m, s_u, me
 %TIME_PER_STEP time per step algorithm
 %
 % varargin:
-%   sn_a   --   number of steps of acceleration
-%   sn_c   --   number of steps during moving with constant speed
-%   sn_d   --   number of steps of deceleration
+%   sn_a   --   number of stairsteps of speeding up
+%   sn_c   --   number of stairsteps at constant speed (max speed)
+%   sn_d   --   number of stairsteps of speeding down
 %   pf_i   --   initial pulse frequency
 %   pf_m   --   maximum pulse frequency
-%   s_u    --   unit steps, default value is 1
+%   s_u    --   steps per stairstep, default value is 1
 %   method --   approximation method
 %
 % varargout:
@@ -15,36 +15,47 @@ function [f_list, dt_list] = time_per_step(sn_a, sn_c, sn_d, pf_i, pf_m, s_u, me
 %   dt_list  --  time periods list
   
 % copyright (c) wulx, <gurdy.woo@gmail.com>
-% last modified by wulx, 2013/10/17
+% last modified by wulx, 2013/10/31
+
+% check args
+if any(~isfinite([sn_a, sn_c, sn_d]) | ([sn_a, sn_c, sn_d]<0))
+    error('the number of steps should be finite non-negative.')
+end
+
+if any(~isfinite([pf_i, pf_m]) | ([pf_i, pf_m]<0))
+    error('pulse frequencies should be finite non-negative.')
+end
+
+if ~isinteger(uint8(s_u)) || (s_u<1)
+    error('steps per stairstep should be 8-bit positive integer (1 - 255).')
+end
 
 % default settings
 if nargin < 7, method = 'ideal'; end
 if nargin < 6, s_u = 1; end
 
-sn = sn_a + sn_d - 1;
-f_list = zeros(1, sn); % pre-allocation for better performance
-dt_list = zeros(1, sn); % ditto
+sn = sn_a + sn_d + 1; % number of stairsteps
+
+% number of stairsteps
+s_list = s_u * ones(1, sn);
+s_list(sn_a+1) = sn_c * s_u;
+
+% frequencies list (pre-alloction)
+f_list = nan(1, sn);
 
 % speed up -------------------------------------------------------%
 % a_f -- frequency acceleration
-[a_f, v_0, v_m] = ramp_eq(pf_i, pf_m, sn_a, s_u);
+[a_f, v_0, ~] = ramp_eq(pf_i, pf_m, sn_a, s_u);
 
 % motion formula:
 %   v1^2 - v0^2 = 2*a*S
 % rising edges
 v_a = [v_0 arrayfun(@(n) sqrt(v_0^2 + 2*a_f*n*s_u), 1:sn_a)];
 
-% reference line
-%dt_a = (v_a(2:end) - v_a(1:end-1)) / a_f;
-%t_list = arrayfun(@(n) sum( dt_a(1:n) ), 0:n_a);
-%plot(t_list, v_list, 'k-')
-
 f_list(1:sn_a) = (v_a(1:end-1) + v_a(2:end)) / 2;
-dt_list(1:sn_a-1) = s_u ./ f_list(1:sn_a-1);
 
 % at max speed ---------------------------------------------------%
-%f_list(sn_a) = pf_m;
-dt_list(sn_a) = (sn_c + 2)*s_u / pf_m;
+f_list(sn_a+1) = pf_m;
 
 % speed down -----------------------------------------------------%
 [d_f, u_0, u_m] = ramp_eq(pf_i, pf_m, sn_d, s_u);
@@ -52,14 +63,15 @@ dt_list(sn_a) = (sn_c + 2)*s_u / pf_m;
 % similar to v_a, falling edges
 u_d = [arrayfun(@(i) sqrt(u_m^2 - 2*d_f*(i-1)*s_u), 1:sn_d) u_0];
 
-f_list(sn_a+1:end) = (u_d(2:end-1) + u_d(3:end)) / 2;
-dt_list(sn_a+1:end) = s_u ./ f_list(sn_a+1:end);
+f_list(sn_a+2:end) = (u_d(1:end-1) + u_d(2:end)) / 2;
+% ----------------------------------------------------------------%
+
+% step time sequences
+dt_list = s_list ./ f_list;
 
 if any( strcmp(method, {'round', 'fix'}) ) % round or fix
     f_list = feval(method, f_list);
     
-    s_list = s_u * ones(1, sn);
-    s_list(sn_a) = (sn_c + 2)*s_u;
     dt_list = s_list ./ f_list;
 end
 
